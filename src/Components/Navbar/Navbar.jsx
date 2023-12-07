e117172b1c1937ac2e0b6aaafcdba502a069ceef
Syarifah Balqis N. Dz <143046915+syarifahb@users.noreply.github.com>
2023-12-07 15:29:56 +0800

    up

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import logo from "../Assets/pc planet logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
 
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext} from "react";
 import all_product from "../Components/Assets/all_product";
 
-export const ShopContext= creatContext(null);
+export const ShopContext= createContext(null);
 
 const ShopContextProvider = (props) => {
 
