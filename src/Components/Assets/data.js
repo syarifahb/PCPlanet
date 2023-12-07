00650d9dd724ff3f476c750d68da9a0e30438a72
IkramAru <ikramarullahafif0@gmail.com>
2023-12-07 10:51:20 +0800

    major changes

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_image from './product_1.jpg'
+import p2_image from './product_2.jpg'
+import p3_image from './product_3.jpg'
+import p4_image from './product_4.jpg'
+
+let data_product = [
+  {
+    id:1,
+    name:"Intel Core i9 Gen12",
+    image:p1_image,
+    new_price:100.00,
+    old_price:190.50,
+  },
+  { 
+    id:2,
+    name:"Gigabyte Ultra Durable H410M H V2",
+    image:p2_image,
+    new_price:120.00,
+    old_price:200.50,
+  },
+  { 
+    id:3,
+    name:"AMD Ryzen 5000 Processor",
+    image:p3_image,
+    new_price:155.00,
+    old_price:235.00,
+  },
+  { 
+    id:4,
+    name:"AMD Ryzen 3400G",
+    image:p4_image,
+    new_price:90.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
